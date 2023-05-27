7dfa7134c9e58a37b1cf24e3535e5aa4179e70ce
Daniel Urra <danielurra75@gmail.com>
2023-05-27 16:48:11 -0500

    Add files via upload

---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFirstProgram
+//
+//  Created by mac user on 5/27/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Objective-C!");
+        NSLog(@"My first console App");
+    
+    }
+    return 0;
+}
